9b65c166f99a1084ea59584fc07dbea3ff29a847
vit2604 <dangtrungvuong2020@gmail.com>
2024-08-22 22:35:18 +0700

    Baitap

--- src/kiemtrachuoi/kiemtrachuoi.java
+++ src/kiemtrachuoi/kiemtrachuoi.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class kiemtrachuoi {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Input : ");
+        System.out.println("Input :");
         boolean check = keyboard.hasNext();
         System.out.println("Kiem tra phai chuoi hay khong : "+check)
     }
--- src/kiemtrakytu/kiemtrakytu.java
+++ src/kiemtrakytu/kiemtrakytu.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class kiemtrakytu {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Input : ");
+        System.out.println("Input :");
         boolean check = keyboard.hasNextInt();
         System.out.println("Kiem tra phai ky tu hay khong : "+check);
     }
