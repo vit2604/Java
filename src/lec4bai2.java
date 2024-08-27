8f7c6492ef7814d7e50b2c78f99056d2d7fb094c
Đặng Trung Vương <151427065+vit2604@users.noreply.github.com>
2024-08-27 09:45:57 +0700

    Add files via upload

---
+++ src/lec4bai2.java
@@ -0,0 +1,17 @@
+
+package lec4bai2;
+import java.util.Scanner;
+public class lec4bai2 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Input n: ");
+        int n = keyboard.nextInt();
+        switch(n)
+        {
+            case 5:System.out.println("Five");break;
+            case 4:System.out.println("Four");break;
+            case 3:System.out.println("Three");break;
+            case 2:System.out.println("Two");break;
+            case 1:System.out.println("One");  break;        
+    }
+}}
